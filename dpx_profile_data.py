693ea3a313bd2e858d0558147341c892e33a7a26
realgoodegg <tomisalive@gmail.com>
2024-11-07 10:00:06 +0000

    Add DPX profile data and Mediainfo switch configuration

---
+++ dpx_profile_data.py
@@ -0,0 +1,18 @@
+# Mediainfo ouput switches
+switches = "--Output=JSON"
+
+# Mediainfo @Video output
+validation_map = {
+    "Format":"DPX",
+    "Format_Version":"2.0",
+    "Format_Compression":"Raw",
+    "Format_Settings_Endianness":"Big",
+    "Format_Settings_Packing": "Filled A",
+    "Width":"2048",
+    "Height":"1556",
+    "PixelAspectRatio":"1.000",
+    "DisplayAspectRatio":"1.316",
+    "ColorSpace":"RGB",
+    "BitDepth":"10",
+    "Compression_Mode":"Lossless",
+}
