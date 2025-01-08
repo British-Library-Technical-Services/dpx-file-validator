ce7be770df55e64dc4eb9d89d7617c4b58f587a6
British Library Sound and Vision <66015813+British-Library-Technical-Services@users.noreply.github.com>
2025-01-08 15:05:30 +0000

    Add service message display for DPX validation service startup

---
+++ service_billboard.py
@@ -0,0 +1,9 @@
+
+def start_service_message():
+    service_message = """
+===| DPX Validation Service |===
+
+Please do not close This window until the service has completed.
+"""
+
+    print(service_message)
