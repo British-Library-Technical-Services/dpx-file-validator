f1966ca049a934e2b8f52ae9bf51dc491eddfc7e
British Library Sound and Vision <66015813+British-Library-Technical-Services@users.noreply.github.com>
2025-01-13 17:42:43 +0000

    Removed and updated DPX profile and validation classes to streamline codebase

--- service_billboard.py
+++
@@ -1,9 +0,0 @@
-
-def start_service_message():
-    service_message = """
-===| DPX Validation Service |===
-
-Please do not close This window until the service has completed.
-"""
-
-    print(service_message)
