a341c983d099350279b0de5ab3c6232f3947dd78
British Library Sound and Vision <66015813+British-Library-Technical-Services@users.noreply.github.com>
2025-02-12 14:02:10 +0000

    Enhance SequenceValidator to include path in logging and improve clarity in critical messages

--- validators/dpx_sequence_validator.py
+++ validators/dpx_sequence_validator.py
@@ -4,7 +4,8 @@ import logging
 logger = logging.getLogger(__name__)
 
 class SequenceValidator:
-    def __init__(self, file_list, manifest):
+    def __init__(self, file_list, manifest, path):
+        self.path = path
         self.file_list = file_list
         self.manifest = manifest
         self.line_count = 0
@@ -16,9 +17,9 @@ class SequenceValidator:
                 self.line_count = sum(1 for count in register)
                 
                 if self.line_count != len(self.file_list):
-                    logger.critical(f"Manifest line count mismatch: line count: {self.line_count} != file count: {len(self.file_list)}")
+                    logger.critical(f"Manifest line count mismatch in {self.path}: line count: {self.line_count} != file count: {len(self.file_list)}")
                 else:
-                    logger.info(f"Manifest line count match: line count: {self.line_count} == file count: {len(self.file_list)}")
+                    logger.info(f"Manifest line count match: line count in {self.path}: {self.line_count} == file count: {len(self.file_list)}")
 
         except FileNotFoundError as e:
             logger.error(f"{self.manifest}, {e}")
@@ -29,11 +30,12 @@ class SequenceValidator:
             sequence_count = int(base_count)
         
             for count in self.file_list:
-                file_count = os.path.basename(count).split("_")[5].split(".")[0]
+                basename = os.path.basename(count).split("_")
+                file_count = basename[5].split(".")[0]
                 
                 while sequence_count < int(file_count):
                     self.missing_sequence.append(sequence_count)
-                    logger.critical(f"Missing sequence: {sequence_count}")
+                    logger.critical(f"Missing sequence: {basename[1]}: {sequence_count}")
                     sequence_count += 1
                 
                 sequence_count += 1
